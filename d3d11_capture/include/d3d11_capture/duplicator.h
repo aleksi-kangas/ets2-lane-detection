cf5ee3ce2488d366908d62c8d34bc3a3086ef3ba
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-03-01 21:16:14 +0200

    Duplicator wrapper for D3D11 capture

---
+++ d3d11_capture/include/d3d11_capture/duplicator.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdexcept>
+
+#include <atlbase.h>
+#include <d3d11.h>
+#include <dxgi1_2.h>
+
+#include "d3d11_capture/device.h"
+#include "d3d11_capture/output.h"
+
+namespace d3d11_capture {
+
+class Duplicator {
+ public:
+  Duplicator(Device& device, Output& output);
+
+  bool UpdateFrame();
+
+  void ReleaseFrame();
+
+  [[nodiscard]] ID3D11Texture2D* D3D11Texture2D() const { return d3d11_texture2d_; }
+
+ private:
+  CComPtr<IDXGIOutputDuplication> dxgi_output_duplication_{nullptr};
+  CComQIPtr<ID3D11Texture2D> d3d11_texture2d_{nullptr};
+};
+
+}  // namespace d3d11_capture
