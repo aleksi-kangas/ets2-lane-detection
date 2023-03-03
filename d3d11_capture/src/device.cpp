27bb45978d0117834bd746fc6164f2632ea8e622
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-03-03 17:25:54 +0200

    Fix device creation to use D3D_DRIVER_TYPE_UNKNOWN when providing a DXGI adapter

--- d3d11_capture/src/device.cpp
+++ d3d11_capture/src/device.cpp
@@ -8,17 +8,16 @@ namespace d3d11_capture {
 
 Device::Device(CComPtr<IDXGIAdapter1> dxgi_adapter1) : dxgi_adapter1_{std::move(dxgi_adapter1)} {
   dxgi_adapter1_->GetDesc1(&dxgi_adapter_desc1_);
-
+  // D3D11CreateDevice requires an unknown driver type when a DXGI adapter is provided
+  constexpr D3D_DRIVER_TYPE kDriverType = D3D_DRIVER_TYPE_UNKNOWN;
   constexpr std::array<D3D_FEATURE_LEVEL, 3> feature_levels = {D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0,
                                                                D3D_FEATURE_LEVEL_10_1};
-
   const HRESULT hr =
-      D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_UNKNOWN, nullptr, 0, feature_levels.data(), feature_levels.size(),
+      D3D11CreateDevice(dxgi_adapter1_, kDriverType, nullptr, 0, feature_levels.data(), feature_levels.size(),
                         D3D11_SDK_VERSION, &d3d11_device_, &d3d_feature_level_, &d3d11_device_context_);
   if (FAILED(hr)) {
     throw std::runtime_error{"TODO"};  // TODO
   }
-
   d3d11_device_->GetImmediateContext(&d3d11_device_immediate_context_);
 }
 
