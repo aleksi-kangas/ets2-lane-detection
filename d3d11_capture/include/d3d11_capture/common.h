9cb0f44325b927dc18e883d9247929c6971990da
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-03-03 18:22:43 +0200

    Add a helper for enumerating DXGI adapters

--- d3d11_capture/include/d3d11_capture/common.h
+++ d3d11_capture/include/d3d11_capture/common.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <vector>
+
+#include <atlbase.h>
+#include <dxgi1_2.h>
+
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 
 namespace d3d11_capture {
+
+std::vector<CComPtr<IDXGIAdapter1>> EnumerateDXGIAdapters();
 
 }  // namespace d3d11_capture
---
+++ d3d11_capture/src/common.cpp
@@ -0,0 +1,21 @@
+#include "d3d11_capture/common.h"
+
+namespace d3d11_capture {
+
+std::vector<CComPtr<IDXGIAdapter1>> EnumerateDXGIAdapters() {
+
+  CComPtr<IDXGIFactory1> dxgi_factory1{nullptr};
+  HRESULT hr = CreateDXGIFactory1(__uuidof(IDXGIFactory1), reinterpret_cast<void**>(&dxgi_factory1));
+  if (FAILED(hr)) {
+    return {};
+  }
+  std::vector<CComPtr<IDXGIAdapter1>> dxgi_adapters{};
+  CComPtr<IDXGIAdapter1> dxgi_adapter1{nullptr};
+  for (UINT i = 0; dxgi_factory1->EnumAdapters1(i, &dxgi_adapter1) != DXGI_ERROR_NOT_FOUND; ++i) {
+    dxgi_adapters.push_back(dxgi_adapter1);
+    dxgi_adapter1.Release();
+  }
+  return dxgi_adapters;
+}
+
+}  // namespace d3d11_capture
