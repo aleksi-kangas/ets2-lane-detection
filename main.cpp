233edaf997595f97ad02a037ab1fcbb358c4d9b5
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-03-06 18:47:13 +0200

    Use UFLD V1 in main loop

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 #include "ufld/v1.h"
 
 int main() {
-  auto& camera = d3d11_capture::Factory::Instance().Create();
-  camera.StartCapture();
-
+  auto& d3d11_camera_factory = d3d11_capture::Factory::Instance();
   ufld::v1::LaneDetector lane_detector{ufld::v1::ModelType::kCULane};
 
+  auto& camera = d3d11_camera_factory.Create();
+  camera.StartCapture();
   while (true) {
     std::optional<cv::Mat> frame = camera.GetLatestFrame();
     if (frame) {
-      cv::imshow("Capture", frame.value());
+      const auto lanes = lane_detector.Detect(frame.value());
+      ufld::VisualizeLanes(lanes, frame.value());
+      cv::imshow("Detected Lanes", frame.value());
       if (cv::waitKey(1) == 27) {
         break;
       }
