06b66b0ab8d8b6a4fe3b7e95f42fb96fb85b7b6f
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-07-17 17:17:46 +0300

    Fix warning

--- ufld/test/math_test.cpp
+++ ufld/test/math_test.cpp
@@ -11,7 +11,7 @@ TEST(UfldMath, SoftMax) {
     const xt::xtensor<float, 2> tensor = xt::adapt(a, {1, 4});
     const xt::xtensor<float, 2> result = ufld::math::SoftMax<1>(tensor);
     const xt::xarray<float> expected{
-        {0.00626879, 0.01704033, 0.04632042, 0.93037047}};
+        {0.00626879f, 0.01704033f, 0.04632042f, 0.93037047f}};
     for (std::size_t i = 0; i < expected.shape()[0]; ++i) {
       for (std::size_t j = 0; j < expected.shape()[1]; ++j) {
         EXPECT_NEAR(result(i, j), expected(i, j), 1e-6f);
